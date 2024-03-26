d0a92026d31b69039c7edb76c217a75e87144de6
NS0H <bory1610@gmail.com>
2024-03-26 17:36:18 +0900

    test

--- src/views/pages/home/PageHome.vue
+++ src/views/pages/home/PageHome.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="page">
-    <slot></slot>
+  <div class="page-content">
+    Home page
   </div>
 </template>
 
 <script lang="ts" setup></script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.page-content {
+  padding: unit(20);
+  padding-top: unit(20) + $page-header-height;
+  white-space: pre-wrap;
+}
+</style>
