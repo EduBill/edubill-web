51fea72821c1876d864885cb76359aa4a933283a
NS0H <bory1610@gmail.com>
2024-03-27 16:19:49 +0900

    feat: SendBillToast 생성

---
+++ src/components/molecules/SendBillToast.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="sendbill" class="sendbill">
+        
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+#sendbill {
+    width: 100%;
+    height: 2.75rem;
+    padding: 0rem 1.25rem;
+    margin-top: 2.25rem;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    
+    border-radius: 0.625rem;
+    background: var(--Primary-Gradient, linear-gradient(90deg, #4600DD 0%, #7535FF 100%));
+}
+</style>
