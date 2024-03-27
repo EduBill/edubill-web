6a59670a344be499aa3fca66b5dfdf7fc13ad051
NS0H <bory1610@gmail.com>
2024-03-27 16:18:13 +0900

    feat: 홈 화면 수정

--- src/views/pages/home/PageHome.vue
+++ src/views/pages/home/PageHome.vue
@@ -1,15 +1,36 @@
 <template>
+  <HomeNav />
+
   <div class="page-content">
-    Home page
+
+    <div>
+      <strong>이름이름(학원학원학원)</strong>님,<br />반갑습니다.
+    </div>
+    <SendBillToast />
+  
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import HomeNav from '@/components/commons/navigation/HomeNav.vue';
+import SendBillToast from '@/components/molecules/SendBillToast.vue';
+</script>
 
 <style lang="scss" scoped>
 .page-content {
   padding: unit(20);
-  padding-top: unit(20) + $page-header-height;
+  // padding-top: unit(20);
   white-space: pre-wrap;
+
+  div {
+    padding-top: 1rem;
+    color: #000;
+
+    /* Title/Title Strong */
+    font-size: 1.625rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 140%; /* 2.275rem */
+  }
 }
 </style>
