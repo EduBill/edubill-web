d0af1eaf7c3833414a54ac97fb449bfd31535020
diglowc <81141945+diglowc@users.noreply.github.com>
2024-04-01 00:31:23 +0900

    feat: 홈 화면에 납부 현황 추가

--- src/views/pages/home/PageHome.vue
+++ src/views/pages/home/PageHome.vue
@@ -2,23 +2,28 @@
   <HomeNav />
 
   <div class="page-content">
-
     <div>
-      <strong>{{ userName }}</strong>님,<br />반갑습니다.
+      <strong>{{ userName }}</strong
+      >님,<br />반갑습니다.
     </div>
     <SendBillToast />
-  
+    <CurrentPaymentStatus
+      title="xx년 xx월 납부 현황"
+      :progressPercent="60"
+      :completeNum="22"
+      iconImg="payment"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
 import HomeNav from '@/components/commons/navigation/HomeNav.vue';
 import SendBillToast from '@/components/molecules/SendBillToast.vue';
+import CurrentPaymentStatus from '@/components/resources/payment/CurrentPaymentStatus.vue';
 import { ref } from 'vue';
 
 // 사용자 이름
 const userName = ref('이름이름(학원학원학원)');
-
 </script>
 
 <style lang="scss" scoped>
