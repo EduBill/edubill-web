0edb47357f341c065a6620d19626bf5057ce2860
diglowc <81141945+diglowc@users.noreply.github.com>
2024-04-01 01:54:43 +0900

    feat: 홈 화면에 관리 메뉴, 안내 메뉴 버튼 추가

--- src/views/pages/home/PageHome.vue
+++ src/views/pages/home/PageHome.vue
@@ -7,18 +7,47 @@
       >님,<br />반갑습니다.
     </div>
     <SendBillToast />
-    <CurrentPaymentStatus
-      title="xx년 xx월 납부 현황"
-      :progressPercent="60"
-      :completeNum="22"
-      iconImg="payment"
-    />
+    <div>
+      <CurrentPaymentStatus
+        title="xx년 xx월 납부 현황"
+        :progressPercent="60"
+        :completeNum="22"
+        iconImg="payment"
+      />
+    </div>
+    <div>
+      <RectangleMenuButton
+        title="납부 관리"
+        content="시간별 납부 내역 확인"
+        iconImg="wallet"
+        backColor="#EEF5FF"
+      />
+      <RectangleMenuButton
+        title="원생 관리"
+        content="시간별 납부 내역 확인"
+        iconImg="chart"
+        backColor="#F3F3FF"
+      />
+    </div>
+    <div>
+      <RectangleMenuButton
+        title="서비스 안내"
+        content="에듀빌만의 서비스"
+        backColor="#F4F4F7"
+      />
+      <RectangleMenuButton
+        title="고객센터"
+        content="고객센터 안내"
+        backColor="#F4F4F7"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import HomeNav from '@/components/commons/navigation/HomeNav.vue';
 import SendBillToast from '@/components/molecules/SendBillToast.vue';
+import RectangleMenuButton from '@/components/resources/buttons/RectangleMenuButton.vue';
 import CurrentPaymentStatus from '@/components/resources/payment/CurrentPaymentStatus.vue';
 import { ref } from 'vue';
 
