71429ccb1bd4a664e3afdbc3dda39be255f87894
diglowc <81141945+diglowc@users.noreply.github.com>
2024-04-23 19:32:28 +0900

    feat: 납부 관리 페이지 상단 nav 생성

---
+++ src/components/commons/navigation/PayManageNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="payManageNav">
+    <svg-icon class="chevronLeft" name="largeChevronLeft" />
+    <p>납부 관리</p>
+    <svg-icon class="plus" name="plusOutline" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import SvgIcon from '@/plugins/svg-icon/lib/SvgIcon.vue';
+</script>
+
+<style lang="scss" scoped>
+.payManageNav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  p {
+    // 다른 요소에 관계없이 중앙에 배치
+    position: absolute;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    color: var(--Gray99, #1a1a1a);
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+}
+
+.chevronLeft {
+  width: unit(32);
+  height: unit(32);
+}
+
+.plus {
+  width: unit(24);
+  height: unit(24);
+}
+</style>
