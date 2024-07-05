58e5b7d160c2eb4abf23eb75175e46c4469d5585
hyewon <wonny2327@gamil.com>
2024-07-05 21:31:12 +0900

    feat:modal 공통컴포넌트

---
+++ src/components/modules/modal/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+  <teleport to="#teleport-area">
+    <div v-if="props.useModal" class="modal-bg" @click="closeModal">
+      <div class="modal-content" @click.stop>
+        <slot></slot>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  useModal: {
+    type: Boolean,
+    default: true,
+  },
+});
+const emits = defineEmits(['close']);
+function closeModal() {
+  setTimeout(() => {
+    emits('close');
+  }, 10);
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-bg {
+  position: fixed;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1000;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/views/pages/studentManage/AddNewClassPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">studentManagePage</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
