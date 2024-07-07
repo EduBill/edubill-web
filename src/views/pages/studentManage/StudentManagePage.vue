4e390487e623bc9d231691c7de1d5de70fb34442
hyewon <wonny2327@gamil.com>
2024-07-07 17:32:02 +0900

    design:studentInfoModal

--- src/views/pages/studentManage/StudentManagePage.vue
+++ src/views/pages/studentManage/StudentManagePage.vue
@@ -2,15 +2,19 @@
   <div class="container">
     studentManagePage
     <div @click="handleModalClick">클릭해봐</div>
-    <Modal :use-modal="useModal" @close="handleModalClick">
-      <div class="modal-content">모달ㅇ디지롱</div>
-    </Modal>
+    <StudentInfoModal
+      :use-modal="useModal"
+      :handle-modal-click="handleModalClick"
+    ></StudentInfoModal>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import Modal from '../../../components/modules/modal/Modal.vue';
+import StudentInfoModal from '../../../components/resources/student/StudentInfoModal.vue';
+import RectangleTextButton from '@/components/resources/buttons/RectangleTextButton.vue';
+import Buttons from '@/components/resources/buttons/Buttons.vue';
 const useModal = ref(false);
 
 function handleModalClick() {
@@ -34,5 +38,8 @@ function handleModalClick() {
   width: 200px;
   height: 200px;
   background-color: white;
+}
+.button {
+  background-color: #7535ff;
 }
 </style>
